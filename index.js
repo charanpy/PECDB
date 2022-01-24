c354aa928f9653952557c25b74c28d0e99021a07
Charan <58394919+charanpy@users.noreply.github.com>
2022-01-24 10:11:10 +0530

    added docker setup

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 require('./lib/db')();
 
 app.listen(process.env.PORT || 4000, () => {
-  console.log('Server started');
+  console.log('Server started', process.env.PORT);
 });
